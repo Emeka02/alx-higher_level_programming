627dbbfaf26306664cf88c77d86229deb53755c7
Emeka02 <106751046+Emeka02@users.noreply.github.com>
2022-09-21 05:50:23 -0700

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
