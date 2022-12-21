3d7718c12cc776d33c7a49199043309d49004143
Emeka02 <106751046+Emeka02@users.noreply.github.com>
2022-12-21 19:11:21 +0100

    Create 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+xecutable File  11 lines (11 sloc)  205 Bytes
+
+#!/usr/bin/node
+/**
+ * Represents a rectangle class with width and height attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
